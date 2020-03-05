b7596fd8e30fa048e9957acc25e84756407a8547
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-06 08:42:25 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("side-bar-button").onclick = () => {
+    let bn = document.getElementById("side-bar-button");
+    btn.setAttribute("aria-expanded", btn.getAttribute("aria-expanded" === "true" ? "false" : "true"));
+}
