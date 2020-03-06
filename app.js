68a2132fda486585859565956098aeae89f3600d
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-06 11:28:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ let a = "aria-expanded";
 let sb = document.getElementById("side-bar-button");
 let sm = document.getElementById("site-mask");
 let b2 = document.getElementById("box2");
-sb.onclick = sm.onclick = a;
-function a() {
+sb.onclick = sm.onclick = s;
+function s() {
     if(sb.getAttribute(a) === "true") {
         sb.setAttribute(a, "false");
         sm.style.zIndex = "-1";
