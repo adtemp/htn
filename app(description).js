27f3e089739595c8df6030765f4478dee77e80b9
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-06 19:10:28 +0900

    Create app(description).js

---
+++ app(description).js
@@ -0,0 +1,32 @@
+/* google adsense を縮小 */
+let adg = () => {
+    let a = document.querySelector(".entry-header");
+    let b = (a.clientWidth - 34) / 300;
+    let c = document.querySelectorAll("div[id^='adg-slot']");
+    let d = document.querySelectorAll("iframe[id^='adg-slot']");
+    for(let i = 0; i < c.length; i++) {
+        c[i].style.marginBottom = (b - 1) * 140 + "%";
+        d[i].style.transform = "scale(" + b + ")";
+    };
+};
+adg();
+
+/* サイドバーボタン */
+let sb = document.getElementById("side-bar-button");
+sb.onclick = sm.onclick = () => {
+    let a = "aria-expanded";
+    let bd = document.querySelector("body");
+    let sm = document.getElementById("site-mask");
+    let b2 = document.getElementById("box2");
+    if(sb.getAttribute(a) === "true") {
+        bd.style.overflow = "auto";
+        sb.setAttribute(a, "false");
+        sm.style.zIndex = "-1";
+        b2.style.right = "-100vw";
+    } else {
+        bd.style.overflow = "hidden";
+        sb.setAttribute(a, "true");
+        sm.style.zIndex = "1";
+        b2.style.right = "0";
+    }
+};
