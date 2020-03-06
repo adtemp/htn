54982c79740b40e41ae7eee058ad9d61ce6c6cb5
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-06 13:08:32 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let a = "aria-expanded";
+let bd = document.querySelector("body");
 let sb = document.getElementById("side-bar-button");
 let sm = document.getElementById("site-mask");
 let b2 = document.getElementById("box2");
 sb.onclick = sm.onclick = () => {
     if(sb.getAttribute(a) === "true") {
+        bd.style.overflow = "auto";
         sb.setAttribute(a, "false");
         sm.style.zIndex = "-1";
         b2.style.right = "-100vw";
     } else {
+        bd.style.overflow = "hidden";
         sb.setAttribute(a, "true");
         sm.style.zIndex = "1";
         b2.style.right = "0";
