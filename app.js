9104df537a492caf224bc8862b22365bb7fa7dfc
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-06 09:12:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 let b = document.getElementById("aria-expanded");
 b.onclick = function() {
-    this.setAttribute("aria-expanded", this.getAttribute("aria-expanded") === "true" ? "false" : "true");
+    this.setAttribute("aria-expanded", b.getAttribute("aria-expanded") === "true" ? "false" : "true");
 };
