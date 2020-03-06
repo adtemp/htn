74a65217c868925106703817363799e741232cef
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-06 19:07:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-let a = "aria-expanded";
-let bd = document.querySelector("body");
+let adg = () => {
+    let a = document.querySelector(".entry-header");
+    let b = (a.clientWidth - 34) / 300;
+    let c = document.querySelectorAll("div[id^='adg-slot']");
+    let d = document.querySelectorAll("iframe[id^='adg-slot']");
+    for(let i = 0; i < c.length; i++) {
+        c[i].style.marginBottom = (b - 1) * 140 + "%";
+        d[i].style.transform = "scale(" + b + ")";
+    };
+};
+adg();
+
 let sb = document.getElementById("side-bar-button");
-let sm = document.getElementById("site-mask");
-let b2 = document.getElementById("box2");
 sb.onclick = sm.onclick = () => {
+    let a = "aria-expanded";
+    let bd = document.querySelector("body");
+    let sm = document.getElementById("site-mask");
+    let b2 = document.getElementById("box2");
     if(sb.getAttribute(a) === "true") {
         bd.style.overflow = "auto";
         sb.setAttribute(a, "false");
