f0213b80492be7e463c3f3c5d1c6421102935b82
adtemp <61062427+adtemp@users.noreply.github.com>
2020-03-11 10:47:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-let adg = () => {
+let sb = document.getElementById("side-bar-button");
+let sm = document.getElementById("site-mask");
+sb.onclick = sm.onclick = () => {
+    let a = "aria-expanded";
+    let bd = document.querySelector("body");
+    let b2 = document.getElementById("box2");
+    if(sb.getAttribute(a) === "true") {
+        sb.setAttribute(a, "false");
+        bd.style.overflow = "auto";
+        sm.style.zIndex = "-1";
+        b2.style.right = "-100vw";
+    } else {
+        sb.setAttribute(a, "true");
+        bd.style.overflow = "hidden";
+        sm.style.zIndex = "1";
+        b2.style.right = "0";
+    }
+};
+
+let tt = document.getElementById("theme-toggle");
+tt.onclick = () => {
+    let tm = document.getElementById("theme");
+    if(tt.getAttribute("switch") === "on") {
+        tm.setAttribute("href", "https://adtemp.github.io/style.css");
+        tt.setAttribute("switch", "off");
+        document.cookie = "theme=light;path=/";
+    } else {
+        tm.setAttribute("href", "https://adtemp.github.io/style-dark.css");
+        tt.setAttribute("switch", "on");
+        document.cookie = "theme=dark;path=/";
+    }
+};
+
+window.onload = () => {
     let a = document.querySelector(".entry-header");
     let b = (a.clientWidth - 34) / 300;
     let c = document.querySelectorAll("div[id^='adg-slot']");
@@ -7,24 +40,4 @@ let adg = () => {
         c[i].style.marginBottom = (b - 1) * 140 + "%";
         d[i].style.transform = "scale(" + b + ")";
     };
-};
-adg();
-
-let sb = document.getElementById("side-bar-button");
-let sm = document.getElementById("site-mask");
-sb.onclick = sm.onclick = () => {
-    let a = "aria-expanded";
-    let bd = document.querySelector("body");
-    let b2 = document.getElementById("box2");
-    if(sb.getAttribute(a) === "true") {
-        bd.style.overflow = "auto";
-        sb.setAttribute(a, "false");
-        sm.style.zIndex = "-1";
-        b2.style.right = "-100vw";
-    } else {
-        bd.style.overflow = "hidden";
-        sb.setAttribute(a, "true");
-        sm.style.zIndex = "1";
-        b2.style.right = "0";
-    }
 };
